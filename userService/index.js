ce76e161519e15cc3da7bb6e7a826457f868d0f8
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-06-03 08:04:23 +0100

    register services: login, getUser, verifyUser

--- userService/index.js
+++ userService/index.js
@@ -3,18 +3,19 @@ require("dotenv").config();
 const grpc = require("@grpc/grpc-js");
 const services = require("./proto/user_grpc_pb");
 
-const API = require("./api");
+const UserServiceImp = require("./serviceImp");
 const connectDB = require("./utils/db");
-
-let api = null;
 
 async function main() {
   const db = await connectDB();
-  api = new API(db, grpc);
+  const userServiceImp = new UserServiceImp(db, grpc);
 
   let server = new grpc.Server();
   server.addService(services.UserServiceService, {
-    signup: api.signup,
+    signup: userServiceImp.signup,
+    login: userServiceImp.login,
+    verifyUser: userServiceImp.verifyUser,
+    getUser: userServiceImp.getUser,
   });
 
   server.bindAsync(
